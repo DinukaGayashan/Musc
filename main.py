7265035648f34c6f4c6f31368c289c6afd04d92b
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2023-09-12 16:38:01 +0530

    add music generator model

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import streamlit as st
+from generators import generator
+import numpy as np
 
-st.write("""
-# Musc
-music from nowhere
-""")
+def function():
+    data = generator.generate_music()
+    st.audio(data, sample_rate=44100)
+
+    sample_rate = 44100  # 44100 samples per second
+    seconds = 2  # Note duration of 2 seconds
+
+    frequency_la = 440  # Our played note will be 440 Hz
+
+    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+    t = np.linspace(0, seconds, seconds * sample_rate, False)
+
+    # Generate a 440 Hz sine wave
+    note_la = np.sin(frequency_la * t * 2 * np.pi)
+    # print(type(note_la))
+    # print(note_la)
+    st.audio(note_la, sample_rate=sample_rate)
+
+    st.write("""
+    hellow
+    """)
+
+
+if __name__ == "__main__":
+    st.title("Musc")
+    st.write("""
+    music from nowhere
+    """)
+
+    if st.button("Generate"):
+        function()
 
 
---
+++ ui/homepage.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def function():
+    print("Hello")
+
+#
+# st.write("""
+# # Musc
+# music from nowhere
+# """)
+#
+# st.button("press", on_click=function())
