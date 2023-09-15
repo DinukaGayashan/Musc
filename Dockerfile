c6618dcf30208946ef06eba8a3864765396b1c09
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2023-09-15 15:01:19 +0000

    dockerizing

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:22.04
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get install -y \
+    git \
+    curl \
+    software-properties-common \
+    && add-apt-repository ppa:deadsnakes/ppa \
+    && apt install -y python3.10 \
+    && apt install -y python3.10-distutils \
+    && apt-get install -y fluidsynth \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /workspace
+RUN curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+
+COPY . .
+
+RUN python3.10 -m pip install tensorflow keras numpy scipy streamlit pretty_midi pyfluidsynth mido
--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import streamlit as st
-from generators import generator
-import numpy as np
+from generators import melody_generator
 
 
 def function():
-    data = generator.generate_music()
-    st.audio(data, sample_rate=44100)
-
-    sample_rate = 44100  # 44100 samples per second
-    seconds = 2  # Note duration of 2 seconds
-
-    frequency_la = 440  # Our played note will be 440 Hz
-
-    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-    t = np.linspace(0, seconds, seconds * sample_rate, False)
-
-    # Generate a 440 Hz sine wave
-    note_la = np.sin(frequency_la * t * 2 * np.pi)
-    print(type(note_la))
-    print(note_la)
-    st.audio(note_la, sample_rate=sample_rate)
-
-    st.write("""
-    hellow
-    """)
+    data = melody_generator.generate_music()
+    st.audio(data)
 
 
 if __name__ == "__main__":
