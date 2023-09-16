e8f6f4d57d983b515ca2415254d80017ef1dab5a
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2023-09-16 13:58:26 +0000

    add spinner

--- ui/homepage.py
+++ ui/homepage.py
@@ -2,6 +2,7 @@ import streamlit as st
 from generators import melody_generator
 from models import model
 
+
 def generate_melody():
     audio_file = melody_generator.generate_music()
     st.audio(audio_file)
@@ -18,7 +19,8 @@ def homepage():
     """)
 
     if st.button("Generate"):
-        generate_melody()
+        with st.spinner('Hold on for a new melody'):
+            generate_melody()
 
     if st.button("Train Models"):
         train_models()
