63db667ed941d68746a71d552ebc3d6d4faf3718
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2023-09-18 14:29:01 +0000

    ux improvement

--- ui/model_page.py
+++ ui/model_page.py
@@ -8,6 +8,7 @@ def train_models(model_name):
     st.success(
         f"Successfully generated a model named {model_name}.")
 
+
 def model_page():
     with st.container():
         st.title("Models")
@@ -15,12 +16,11 @@ def model_page():
             "Generate music generating models with custom datasets. Provide a dataset and Train the models and manage them.")
         st.divider()
 
-    with st.container():
-        # left_col, right_col = st.columns([1, 1])
-        with st.container():
-            model_names = utility.get_model_names()
-            model = st.radio("Model", model_names,)
-        st.divider()
+    # with st.container():
+    #     with st.container():
+    #         model_names = utility.get_model_names()
+    #         model = st.radio("Model", model_names,)
+    #     st.divider()
 
     with st.container():
         left_col, right_col = st.columns([2, 3], gap="large")
@@ -29,14 +29,17 @@ def model_page():
         with right_col.container():
             uploaded_files = st.file_uploader(
                 "Choose midi files", accept_multiple_files=True, type="mid")
-
+        st.write("#")
     with st.container():
-        generate=st.button("Generate")
+        generate = st.button("Generate")
         st.divider()
         if generate:
             if model_name and uploaded_files:
-                utility.save_dataset(model_name, uploaded_files)
-                with st.spinner("Hold on for a new model"):
-                    train_models(model_name)
+                if not utility.is_model_available(model_name):
+                    utility.save_dataset(model_name, uploaded_files)
+                    with st.spinner("Hold on for a new model"):
+                        train_models(model_name)
+                else:
+                    st.warning("Model with same name already exists.")
             else:
                 st.warning("Enter a model name and a dataset.")
--- ui/utility.py
+++ ui/utility.py
@@ -7,25 +7,29 @@ def get_model_names():
     return files
 
 
-def save_dataset(dataset_name,uploaded_files):
-    folder_path=f"datasets/{dataset_name}"
+def is_model_available(model_name):
+    return get_model_names().count(model_name) > 0
+
+
+def save_dataset(dataset_name, uploaded_files):
+    folder_path = f"datasets/{dataset_name}"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    
+
     for uploaded_file in uploaded_files:
-            with open(os.path.join(folder_path, uploaded_file.name), "wb") as f:
-                f.write(uploaded_file.getvalue())
+        with open(os.path.join(folder_path, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getvalue())
 
 
 def save_model(name, model):
-    folder_path="models/trained_models"
+    folder_path = "models/trained_models"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     model.save(f"models/trained_models/{name}.keras")
-    
+
 
 def save_melody(name, file):
-    folder_path="generated_melodies"
+    folder_path = "generated_melodies"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     file.save(name)
